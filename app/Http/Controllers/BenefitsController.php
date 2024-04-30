e08da275ef63d0f94facb601eec6e833045c36c1
Pablo Quiroz <pabloskiquiroz@gmail.com>
2024-04-30 13:24:24 -0400

    Agrega documentación swagger

--- app/Http/Controllers/BenefitsController.php
+++ app/Http/Controllers/BenefitsController.php
@@ -4,6 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Http;
 
+/**
+ * @OA\Info(
+ *             title="Beneficios agrupados por año, ya filtrados",
+ *             version="1.0",
+ *             description="Prueba tecnica, consumo de end points: benficios, filtros y fichas. Se filtra cada beneficio segun su filtro asociado, para agruparlos por año"
+ * )
+ *
+ * @OA\Server(url="http://localhost")
+ */
 class BenefitsController extends Controller
 {
     private $benefits;
@@ -24,8 +33,116 @@ class BenefitsController extends Controller
         $this->dataSheet = collect($dataSheetRes['data']);
     }
 
-    public function yearBenefits() {
-
+    /**
+     * Beneficios filtrados y con su ficha, agrupados por año.
+     *
+     * @OA\Get(
+     *     path="/yearBenefits",
+     *     tags={"Beneficios"},
+     *     summary="Filtra los beneficios y se les asigna una ficha, agrupando los beneficios por año.",
+     *
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK",
+     *
+     *         @OA\JsonContent(
+     *
+     *             @OA\Property(
+     *                 property="code",
+     *                 type="integer",
+     *                 example=200
+     *             ),
+     *             @OA\Property(
+     *                 property="success",
+     *                 type="boolean",
+     *                 example=true
+     *             ),
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="array",
+     *
+     *                 @OA\Items(
+     *
+     *                     @OA\Property(
+     *                         property="year",
+     *                         type="integer",
+     *                         example=2023
+     *                     ),
+     *                     @OA\Property(
+     *                         property="num",
+     *                         type="integer",
+     *                         example=8
+     *                     ),
+     *                     @OA\Property(
+     *                         property="beneficios",
+     *                         type="array",
+     *
+     *                         @OA\Items(
+     *
+     *                             @OA\Property(
+     *                                 property="id_programa",
+     *                                 type="integer",
+     *                                 example=147
+     *                             ),
+     *                             @OA\Property(
+     *                                 property="monto",
+     *                                 type="integer",
+     *                                 example=40656
+     *                             ),
+     *                             @OA\Property(
+     *                                 property="fecha_recepcion",
+     *                                 type="string",
+     *                                 example="09/11/2023"
+     *                             ),
+     *                             @OA\Property(
+     *                                 property="fecha",
+     *                                 type="string",
+     *                                 example="2023-11-09"
+     *                             ),
+     *                             @OA\Property(
+     *                                 property="ano",
+     *                                 type="string",
+     *                                 example="2023"
+     *                             ),
+     *                             @OA\Property(
+     *                                 property="ficha",
+     *                                 type="object",
+     *                                 @OA\Property(
+     *                                     property="id",
+     *                                     type="integer",
+     *                                     example=922
+     *                                 ),
+     *                                 @OA\Property(
+     *                                     property="nombre",
+     *                                     type="string",
+     *                                     example="Emprende"
+     *                                 ),
+     *                                 @OA\Property(
+     *                                     property="url",
+     *                                     type="string",
+     *                                     example="emprende"
+     *                                 ),
+     *                                 @OA\Property(
+     *                                     property="categoria",
+     *                                     type="string",
+     *                                     example="trabajo"
+     *                                 ),
+     *                                 @OA\Property(
+     *                                     property="descripcion",
+     *                                     type="string",
+     *                                     example="Fondos concursables para nuevos negocios"
+     *                                 )
+     *                             )
+     *                         )
+     *                     )
+     *                 )
+     *             )
+     *         )
+     *     )
+     * )
+     */
+    public function yearBenefits()
+    {
         $data = collect();
 
         $this->benefits->map(function ($value, $key) {
